a2ea4f8a2918be3892b4b2d4bf560e8d554294d8
vikas <vikas@avigma.com>
2015-07-25 18:17:16 +0530

    changes in add friend and account.

---
+++ JiE/JiE/AccountsViewController.h
@@ -0,0 +1,13 @@
+//
+//  AccountsViewController.h
+//  JiE
+//
+//  Created by vikas on 25/07/15.
+//  Copyright (c) 2015 avigma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AccountsViewController : UIViewController
+
+@end
---
+++ JiE/JiE/FriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendsViewController.h
+//  JiE
+//
+//  Created by vikas on 25/07/15.
+//  Copyright (c) 2015 avigma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendsViewController : UIViewController
+
+@end
