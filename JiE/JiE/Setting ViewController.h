cc262204d18181681ccf5d2d86acb27b67ec661f
vikas <vikas@avigma.com>
2015-07-28 19:52:53 +0530

    changes in design

---
+++ JiE/JiE/Setting ViewController.h
@@ -0,0 +1,13 @@
+//
+//  Setting ViewController.h
+//  JiE
+//
+//  Created by vikas on 28/07/15.
+//  Copyright (c) 2015 avigma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Setting_ViewController : UIViewController
+
+@end
